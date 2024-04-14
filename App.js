bb942bd9f5f3f38db06dde02f55d273285a431b4
PhimphixayNouhak <140489606+PhimphixayNouhak@users.noreply.github.com>
2024-04-14 22:06:34 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Helloworld from './src/Helloworld';
+import Capturingtap from './src/Capturingtap';
+import Customcomponent from './src/Customcomponent';
+import Stateprop from './src/Stateprop';
+import Styling from './src/Styling';
+import Scrollablecotent from './src/Scrollablecotent';
+import Buildingaform from './src/Buildingaform';
+import Longlists from './src/Longlists';
+import Cacualator from './src/Caculator';
+export default function App() {
+  return (
+      <Cacualator/>
+  );
+}
+
+
